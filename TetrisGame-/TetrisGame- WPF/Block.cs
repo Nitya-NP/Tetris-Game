b6c315fce41b7a94dbb1c0b7f7ca60a610a3dcb0
Nitya-NP <117617150+Nitya-NP@users.noreply.github.com>
2025-09-07 14:57:33 -0500

    Add files via upload

---
+++ TetrisGame-/TetrisGame- WPF/Block.cs
@@ -0,0 +1,70 @@
+﻿//Tetris Game 
+// Nitya Patel 
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisGame__WPF
+{
+    // Abstract class for Tetris blocks
+    // Each specific block (I, O, T, etc.) will inherit from this
+    public abstract class Block
+    {
+        public abstract int ID { get; }// for the block number
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffSet {get; }
+        private Position offSet;
+        private int rotation;
+
+
+        public Block()
+        {
+            offSet = new Position(StartOffSet.Row, StartOffSet.Col);
+            rotation = 0;
+         
+        }
+
+        public void RotateCW()
+        {
+
+            rotation = (rotation + 1) % Tiles.Length;
+
+        }
+
+        public void RotateCCW()
+        {
+            if (rotation == 0)
+                rotation = 3;
+            else rotation--;
+
+
+        }
+
+        public void Move(int rows, int cols)
+        {
+            offSet.Row += rows;
+            offSet.Col += cols;
+        }
+
+        public void Reset()
+        {
+            offSet.Row = StartOffSet.Row;
+            offSet.Col = StartOffSet.Col;
+            rotation = 0;
+        }
+
+        
+        public IEnumerable<Position> TilePositions() //it's read only
+        {
+            foreach(Position p in Tiles[rotation])
+            {
+                yield return new Position(p.Row + offSet.Row, p.Col + offSet.Col);
+            }
+        }
+
+    }
+}
---
+++ TetrisGame-/TetrisGame- WPF/TBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TetrisGame__WPF
+{
+    // Represents the "T" shaped Tetris block
+    public class TBlock : Block
+    {
+        private Position[][] tiles = new Position[][]
+         {
+            new Position[]{new Position(0,1), new Position(1,0), new Position(1,1),new Position(1,2)},
+            new Position[]{new Position(0,1) ,new Position(1,1),new Position(1,2),new Position(2,1)},
+            new Position[]{new Position(1,0) ,new Position(1,1),new Position(1,2),new Position(2,1)},
+            new Position[]{new Position(0,1) ,new Position(1,0),new Position(1,1),new Position(2,1)}
+
+        };
+
+        public override int ID => 6;
+        protected override Position StartOffSet => new Position(0, 3);
+        protected override Position[][] Tiles => tiles;
+        
+    }
+}
